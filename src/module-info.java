18f3118e4b4a21de5d7553aac7f6401672a507e8
Carlos Eduardo <91908308+CarlosTertuliano@users.noreply.github.com>
2023-06-21 18:15:06 -0300

    Delete src directory

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module BatalhaNavalLP2 {
-		requires javafx.controls;
-		requires javafx.fxml;
-		requires javafx.graphics;
-		exports br.ufrn.imd;
-		
-		opens application to javafx.graphics, javafx.fxml;
-		opens br.ufrn.imd.controle to javafx.fxml;
-}
